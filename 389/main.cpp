53424bfd44043819f0b1a835f2ae80792498eb51
Joshua D. Thurston <joshuathurston1@yahoo.com>
2023-09-24 21:59:06 -0700

    first

---
+++ 389/main.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/find-the-difference/description/?envType=daily-question&envId=2023-09-25
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> vals(26, 0);
+
+        for(int i = 0; i < t.length(); i++) {
+            vals[t[i]-'a']--;
+            if(i < s.length()) vals[s[i]-'a']++;
+        }
+
+        for(int i = 0; i < vals.size(); i++) {
+            if(vals[i] == -1) return i+'a';
+        }
+        return 'a';
+    }
+};
